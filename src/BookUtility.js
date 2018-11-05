0da38c243f21c9de915b0f8a64d4d24756bc7331
Lindsay Nunez <lindsayanunez@gmail.com>
2018-11-05 08:00:18 -0500

    Take in, sort and return the list of books putting them in Title and Upper Case.

--- src/BookUtility.js
+++ src/BookUtility.js
@@ -1,1 +1,24 @@
-BookUtility.js
+export const sortAllBooks = (list) =>{
+  //Take in, sort and return the list of books
+  const newList = list.sort(function(a ,b){
+
+    const titleA = a
+    .title
+    .toUpperCase();
+
+    const titleB = b
+    .title
+    .toUpperCase();
+
+    if(titleA < titleB{
+      return -1;}
+
+    if (titleA > titleB){
+      return 1;
+    }
+
+    return 0;
+    })
+
+    return newList;
+  }
