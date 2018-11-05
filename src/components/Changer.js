fa94de50cba0a387ca0cc8e5d28eaeecbca2c34d
Lindsay Nunez <lindsayanunez@gmail.com>
2018-11-04 19:35:12 -0500

    Moved changer code to changer componenent.

--- src/components/Changer.js
+++ src/components/Changer.js
@@ -1,1 +1,9 @@
-Changer.js
+<div className="book-shelf-changer">
+          <select>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
