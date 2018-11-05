cc23db017ca460b06165779a78dcc8f16aecaec6
Lindsay Nunez <lindsayanunez@gmail.com>
2018-11-05 08:33:22 -0500

    map over the shelves with a null check to create as many shelves as exist.

--- src/components/BookCase.js
+++ src/components/BookCase.js
@@ -14,7 +14,41 @@ class BookCase extends Component{
     .onRefreshAllBooks();
   }
 
+  updateShelves = () =>{
+    //Updates state of the shelves to show the correct books.
+
+    const newCurrentlyReading ={
+      name: "Currently Reading",
+      books: this
+        .props
+        .books
+        .filter(book => book.shelf === "currentlyReading")
+
+    };
+    const newWantToRead ={
+      name: "Want to Read",
+      books: this
+        .props
+        .books
+        .filter(book => book.shelf === "wantToRead")
+
+    };
+    const newRead ={
+      name: "Read",
+      books: this
+        .props
+        .books
+        .filter(book => book.shelf === "read")
+    };
+  }
+
   render()
+  //nullcheck to see
+  //how many books you have it will update the shelves
+  let shelves = [];
+  if (this.props.books && this.props.books.length)
+    shelves =this.updateShelves();
+
   return(
     <div className="app">
           <div className="list-books">
@@ -23,7 +57,12 @@ class BookCase extends Component{
             </div>
             <div className="list-books-content">
               <div>
-                <BookShelf />
+              //map over theshelves with a null check to create as may cshelves as exist
+              {shelves && shelves.map(shelf) => (
+                <BookShelf
+                key={shelf.name}
+                shelf={shelf}/>)}
+
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
