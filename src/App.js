b5cf8378033daa4dd2820e22ee40bba5d1899a28
Lindsay Nunez <lindsayanunez@gmail.com>
2018-11-04 19:07:22 -0500

    Imported the BookCase Component to App JS.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import BookCase from './components/BookCase';
 
 class BooksApp extends React.Component {
   state = {
