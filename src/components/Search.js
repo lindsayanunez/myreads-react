64545f3184ba1a6d322d4887bd2947f76b6b0ed4
Lindsay Nunez <lindsayanunez@gmail.com>
2018-11-07 20:18:07 -0500

    Search is now ready for react router.

--- src/components/Search.js
+++ src/components/Search.js
@@ -58,7 +58,7 @@ componentRecievesProps = (props) => {
     return(
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link className="close-search" to='/')>Close</a>
           <div className="search-books-input-wrapper">
             <input type="text" placeholder="Search by title or author"/>
           </div>
