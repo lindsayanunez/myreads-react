b4a7d606dbce3628d9c40aa915d7f4bba5daed1b
Lindsay Nunez <lindsayanunez@gmail.com>
2018-11-07 20:22:58 -0500

    Updated the input field to change the state of the query based on what is typed.

--- src/components/Search.js
+++ src/components/Search.js
@@ -58,12 +58,15 @@ componentRecievesProps = (props) => {
     return(
       <div className="search-books">
         <div className="search-books-bar">
-          <Link className="close-search" to='/')>Close</a>
+          <Link className="close-search" to='/')>Close</Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <input type="text" placeholder="Search by title or author"
+            onChange={(e)=>this.newQuery(e.target.value)}
+            value={this.state.query.value}/>
           </div>
         </div>
         <div className="search-books-results">
+
           <ol className="books-grid"></ol>
         </div>
       </div>
