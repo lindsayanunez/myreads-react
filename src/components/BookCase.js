a5b3e50637ec1441f2b0a42c857a0f89057a647a
Lindsay Nunez <lindsayanunez@gmail.com>
2018-11-12 18:20:19 -0600

    Adjusted syntax.

--- src/components/BookCase.js
+++ src/components/BookCase.js
@@ -1,77 +1,64 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
 // import * as BooksAPI from './BooksAPI'
 import BookShelf from './BookShelf';
 
 class BookCase extends Component{
-  state ={
-
-  }
+  state ={};
 
   componentDidMount = () =>{
     //Updates list of allthe books
-    this
-    .props
-    .onRefreshAllBooks();
-  }
+    this.props.onRefreshAllBooks();
+  };
 
   updateShelves = () =>{
     //Updates state of the shelves to show the correct books.
 
     const newCurrentlyReading ={
       name: "Currently Reading",
-      books: this
-        .props
-        .books
-        .filter(book => book.shelf === "currentlyReading")
-
+      books: this.props.books.filter(book => book.shelf === "currentlyReading")
     };
     const newWantToRead ={
       name: "Want to Read",
-      books: this
-        .props
-        .books
-        .filter(book => book.shelf === "wantToRead")
-
+      books: this.props.books.filter(book => book.shelf === "wantToRead")
     };
     const newRead ={
       name: "Read",
-      books: this
-        .props
-        .books
-        .filter(book => book.shelf === "read")
+      books: this.props.books.filter(book => book.shelf === "read")
     };
     return([newCurrentlyReading, newWantToRead, newRead]);
-  }
+  };
 
   render(){
   //nullcheck to see
   //how many books you have it will update the shelves
   let shelves = [];
   if (this.props.books && this.props.books.length)
-    shelves =this.updateShelves();
+    shelves = this.updateShelves();
 
   return(
     <div className="app">
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {shelves &&
+              shelves.map((shelf) => (
+              <BookShelf
+              key={shelf.name}
+              shelf={shelf}
+              onswitchShelf={this.props.onSwitchShelf} />))}
             </div>
-            <div className="list-books-content">
-              <div>
-              //map over theshelves with a null check to create as may cshelves as exist
-              {shelves && shelves.map((shelf) => (
-                <BookShelf
-                key={shelf.name}
-                shelf={shelf} onswitchShelf={this.props.onSwitchShelf} />))}
-              </div>
-              </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
+          <div className="open-search">
+            <a onClick={() => this.setState({ showSearchPage: true })}>
+              Add a book
+            </a>
           </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 export default BookCase;
