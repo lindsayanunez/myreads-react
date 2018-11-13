1976ff53543577c2e6947c280853d0e39159b960
Lindsay Nunez <lindsayanunez@gmail.com>
2018-11-12 18:25:45 -0600

    Adjusted syntax.

--- src/components/Changer.js
+++ src/components/Changer.js
@@ -4,37 +4,38 @@ import React, {Component} from 'react'
 class Changer extends Component{
   state = {
     shelfSelection: this.props.book.shelf || "none"
-  }
+  };
 
 onSwitchShelf = (book, shelf) => {
   //Changes the state for the change of shelf and sends call back.
   this.setState({
     shelfSelection: shelf});
   this.props.onSwitchShelf(book,shelf);
-}
+};
 
-componentWillRecieveProps = (props) =>{
+componentWillRecieveProps = props => {
   this.props = props;
   this.setState({
     shelfSelection: this.props.book.shelf
   });
-
-}
+};
 
   render(){
     return(
       <div className="book-shelf-changer">
-        <select>
+        <select
           value={this.state.shelfSelection}
-          onSwitchShelf={(e) => this.onSwitchShelf(this.props.book, e.target.value)}
-          <option value="move" disabled>Move to...</option>
+          onChange={e => this.onSwitchShelf(this.props.book, e.target.value)}>
+          <option value="move" disabled>
+            Move to...
+          </option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
           <option value="none">None</option>
         </select>
       </div>
-    )
+    );
   }
 }
 
