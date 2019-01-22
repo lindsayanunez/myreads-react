d06aed7cd3d0f54486d5a748da5e60f6a79f0ed4
Lindsay Nunez <lindsayanunez@gmail.com>
2019-01-21 19:00:47 -0500

    updated package.json.

---
+++ build/precache-manifest.f6e325f264ae3f5f7ad3439e9e35ac7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads-react/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads-react/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads-react/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3923c7c42fa552ff5f3a",
+    "url": "/myreads-react/static/js/runtime~main.3923c7c4.js"
+  },
+  {
+    "revision": "3d036c38e6f987a9e3e1",
+    "url": "/myreads-react/static/js/main.3d036c38.chunk.js"
+  },
+  {
+    "revision": "1568edc1f8a8b9ccb7e1",
+    "url": "/myreads-react/static/js/1.1568edc1.chunk.js"
+  },
+  {
+    "revision": "3d036c38e6f987a9e3e1",
+    "url": "/myreads-react/static/css/main.9a475a75.chunk.css"
+  },
+  {
+    "revision": "fb8a8eb35634fdf862d2b2f42edfdf5d",
+    "url": "/myreads-react/index.html"
+  }
+];
